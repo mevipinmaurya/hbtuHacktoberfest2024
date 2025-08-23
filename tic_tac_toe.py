6e87f144703a2012dbff77b6667da63685a38f30
Adnan <adnanafzal01@gmail.com>
2025-08-23 12:57:11 +0530

    1. Added option to add player names before playing. 2. The previous code allowed to edit boxes already marked with 'x' or 'o', fixed that. 3. Edited end() to only eturn a winner, added an 'if' condition to address draws.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,78 +8,69 @@ board = [
          ['6','7','8']
         ]
 
+Player1 = input("Enter name for player 1: ")
+Player2 = input("Enter name for player 2: ")
+
 def print_board():
   for i in board:
     print(i)
 
 
 def p1():
-
-  n = int(input("Jeevan's_move.  "))
-  row = n//3
-  column = n%3
-  board[row][column]="X"
-  print_board()
+    n = int(input(f"{Player1}'s move (0-8): "))
+    row, column = divmod(n, 3)
+    if board[row][column] not in ['X', 'O']:  # Prevent overwriting
+        board[row][column] = "X"
+    else:
+        print("Invalid move, try again.")
+        p1()
+    print_board()
 
 def p2():
-
-  n = int(input("Thrishul's_move.  "))
-  row = n//3
-  column = n%3
-  board[row][column]="O"
-  print_board()
+    n = int(input(f"{Player2}'s move (0-8): "))
+    row, column = divmod(n, 3)
+    if board[row][column] not in ['X', 'O']:  # Prevent overwriting
+        board[row][column] = "O"
+    else:
+        print("Invalid move, try again.")
+        p2()
+    print_board()
 
 def end():
-  if board[0][0]==board[1][0]==board[2][0]:
-    return(board[0][0])
 
-  elif board[0][1]==board[1][1]==board[2][1]:
-    return(board[0][1])
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
 
-  elif board[0][2]==board[1][2]==board[2][2]:
-    return(board[0][2])
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
 
-
-  elif board[0][0]==board[0][1]==board[0][2]:
-    return(board[0][0])
-
-  elif board[1][0]==board[1][1]==board[1][2]:
-    return(board[1][1])
-
-  elif board[2][0]==board[2][1]==board[2][2]:
-    return(board[2][0])
-
-
-  elif board[0][0]==board[1][1]==board[2][2]:
-    return(board[0][0])
-
-  elif board[0][2]==board[1][1]==board[2][0]:
-    return(board[0][2])
-
-  else:
-    return("draw")
-
-
+    return None  # means no winner still
 
 
 print_board()
-count=0
-for i in range(5):
-  p1()
-  count+=1
-  a = end()
-  if a=="X":
-    print("Game Ended")
-    print("Jeevan won")
-    break
-  if count==9:
-    print("draw")
-    break
+count = 0
 
-  p2()
-  count+=1
-  a = end()
-  if a=="O":
-    print("Game Ended")
-    print("Thrishul won")
-    break
+for i in range(9):
+    if i % 2 == 0:
+        p1()
+        winner = end()
+        if winner == "X":
+            print("Game Ended")
+            print(f"{Player1} won!")
+            break
+    else:
+        p2()
+        winner = end()
+        if winner == "O":
+            print("Game Ended")
+            print(f"{Player2} won!")
+            break
+    count += 1
+
+if not end():
+    print("Draw.")
