09bb885724a4fc4d5a48b34d085868d0713efcd1
Mohit Mehtre <mohitmehtre@gmail.com>
2025-10-03 13:00:10 +0530

    Add Loader component with CSS animation

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <div className="spinner-loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
